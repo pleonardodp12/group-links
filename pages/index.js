ac6ed66cd7db97a365234ce58db363b2926cc677
pleonardodp12 <pauloleonardo_dp@outlook.com>
2020-10-08 22:25:52 -0300

    test prismic props

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import Prismic from 'prismic-javascript';
 
-const Index = () => {
-  return <h1>Links groups</h1>
+const Index = (props) => {
+  console.log('client')
+  return (
+    <div style={{ backgroundColor: props.data.corfundo, color: props.data.cortexto}}>
+      <h1>Links groups</h1>
+      <pre>{JSON.stringify(props, null, 2)}</pre>
+    </div>
+    
+  );
 };
+
+export async function getServerSideProps(){
+  console.log('server')
+  const client = Prismic.client("https://linksgroup.cdn.prismic.io/api/v2")
+  const centralLinks = await client.getSingle("centrallinks")
+  console.log(centralLinks)
+  return {
+    props: {
+      data: centralLinks.data,
+    }
+  }
+}
 
 export default Index;
