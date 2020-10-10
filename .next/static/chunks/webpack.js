c1c4d9019a23b8b9ce562ebb7d4dc7966b100a00
pleonardodp12 <pauloleonardo_dp@outlook.com>
2020-10-10 19:53:15 -0300

    redirecionamento concluido

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "65d32e1d94a3237d20e7";
+/******/ 	var hotCurrentHash = "154ae78578b6696a9fd8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/[slug].js
+++ pages/[slug].js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Prismic from 'prismic-javascript';
 
 const RedirectTo = () => {
+  useEffect(() => {
+    
+  }, [])
   return (
-    <h1>Redirect</h1>
+    <div>
+      <h1>URL não encontrada</h1>
+      <p>Estamos redirecionando você para a central de links.</p>
+    </div>
   );
 };
 
-export async function getServerSideProps({ params }){
+export async function getServerSideProps({ params, res }){
   const client = Prismic.client("https://linksgroup.cdn.prismic.io/api/v2");
-  const link = await client.getByUID('shortlink', "linkedin");
+  const link = await client.getByUID('shortlink', params.slug);
   if(link){
-    return null
+    res.statusCode = 301; //conteudo movido permanentemente 
+    res.setHeader('Location', link.data.destino.url); //redireciona
+    res.end();
+    return;
   }
   console.log(link)
   return {
